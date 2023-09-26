335422ca067f8b2bfd95bb92460e00b6081e15d4
EC2 Default User <ec2-user@ip-172-31-47-40.ap-northeast-1.compute.internal>
2023-09-26 15:07:30 +0000

    D1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,8 @@ class UsersController < ApplicationController
   end
   
   def update
-    if @user.update_attributes(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
   root 'static_pages#top'
   get '/signup', to: 'users#new'
   
@@ -7,5 +6,7 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
+  resources :users do
+    resources :tasks
+  end 
 end
